b8338e9bdf8a635496b57d7a8196ed1ac2215a9d
Kovacs Alex <alex@atm.moe>
2020-12-01 18:55:53 +0200

    chore: that no newline triggered me

--- 2020/day1/index.mjs
+++ 2020/day1/index.mjs
@@ -24,6 +24,7 @@ In this list, the two entries that sum to 2020 are 1721 and 299. Multiplying the
 
 Of course, your expense report is much larger. Find the two entries that sum to 2020; what do you get if you multiply them together?
 */
+
 import { readFile } from 'fs/promises';
 
 let input = await readFile('./input.txt');
